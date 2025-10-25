0ef41cdd1b61e957916ead7345eba188529fe954
ayan <ayanrasulova13@gmail.com>
2025-10-25 12:01:56 -0400

    fixed initialization

--- src/components/handtracking.py
+++ src/components/handtracking.py
@@ -4,3 +4,7 @@ import cv2
 import os
 import mediapipe as mp
 
+hands_mp = mp.solutions.hands # create hand recognition algorithm object
+hands = hands_mp.Hands() #
+mp_draw = mp.solutions.drawing_utils # draw detected points 
+
