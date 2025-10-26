0dcae46e2e176a9ff356357bfa2b07e48f4024cb
jackawackadoo <jackawackadoo@gmail.com>
2025-10-25 23:34:13 -0400

    got rid of an unnecessary piece of code

--- setup.py
+++ setup.py
@@ -41,9 +41,6 @@ def main():
     else:
         print("No requirements.txt found — skipping pip install.")
 
-    # Install mediapipe using nvm which is installed from dependencies.txt
-    cmd = "nvm install mediapipe"
-    run_list(cmd)
 
 if __name__ == "__main__":
     main()
