d599f463d87262e4a7ac4f62f1eb26c7d3af086a
jackawackadoo <jackawackadoo@gmail.com>
2025-10-26 02:43:48 -0400

    update handlandmarks

--- src/components/handlandmarks.py
+++ src/components/handlandmarks.py
@@ -71,28 +71,62 @@ def swipe(hand_landmarks):
     
     return None
 
-# mouse up
-def point_up(hand_landmarks):
-    thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+# double swipe (changed to this because its too easy to erase canvas)
+def double_swipe(left_hand, right_hand):
+    if left_hand is None or right_hand is None:
+        return False
+    
+    left_swipe = swipe(left_hand)
+    right_swipe = swipe(right_hand)
+
+    # both must swipe in the same direction within the same frame
+    if left_swipe and right_swipe:
+        return True
+    return False
+
+def pointer(hand_landmarks, side):
+    # if point and thumb closer to middle tip than index knuckle, 
     index_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
     middle_tip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
     ring_tip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
     pinky_tip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP]
-
-    def dist(a, b): # distance between landmarks helper func
-        return math.hypot(a.x - b.x, a.y - b.y)
-    
+    thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
     index_knuckle = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
-    thumb_ip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP]
 
-    # true if finger tips above thumb
     index_above_thumb = index_tip.y < thumb_tip.y and index_tip.y < index_knuckle.y
     fingers_below_index = middle_tip.y > thumb_tip.y and ring_tip.y > thumb_tip.y and pinky_tip.y > thumb_tip.y
+    thumb_facing_in = thumb_tip.x > index_tip.x
+    thumb_facing_out = thumb_tip.x < index_tip.x
 
-    thumb_index_close = dist(thumb_tip, index_tip) < 0.07  # tweak threshold if needed
-    thumb_inline = abs(thumb_tip.x - index_knuckle.x) < 0.1
+    # reversed based on either left or right (right = left hand (since canvas is reversed))
+    if side == "right":
+        if (index_above_thumb and fingers_below_index and thumb_facing_out):
+            return "point"
+        elif (index_above_thumb and fingers_below_index and thumb_facing_in):
+            return "draw"
+        else:
+            return False
+        
+    if side == "left":
+        if (index_above_thumb and fingers_below_index and thumb_facing_in):
+            return "point"
+        elif (index_above_thumb and fingers_below_index and thumb_facing_out):
+            return "draw"
+        else:
+            return False
 
-    return (index_above_thumb and fingers_below_index and thumb_inline and thumb_index_close) # and thumb_in
+def point_thumb_in(hand_landmarks, side):
+    if pointer(hand_landmarks, side) == "point":
+        return True
+    else:
+        return False
+    
+def point_thumb_out(hand_landmarks, side):
+    if pointer(hand_landmarks, side) == "draw":
+        return True
+    else: 
+        return False
+
 
 # mouse down
 def point_up_thumb_out():
